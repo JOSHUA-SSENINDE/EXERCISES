b0167dc10c0da870cb5b3503a82d7678c2b1cae6
JOSHUA-SSENINDE <joshsseni20@gmail.com>
2024-09-26 13:12:10 +0300

    Update

---
+++ Locating6.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+ // Compiler version JDK 11.0.2
+
+ class Decoder
+ {
+   public static void main(String args[])
+ 
+   { 
+     
+     int[][] matrix = {
+       {1,2,3},
+       {4,5,6},
+       {7,8,9}
+     };
+    System.out.println("Element:" +matrix[1][2]);
+   }
+ }
